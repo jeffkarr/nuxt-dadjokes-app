3da465cb8a2ed833247f0d00681628007d26eff6
Jeff Karr <jeff.karr@digitalallyinc.com>
2021-06-12 16:21:21 -0500

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>This is an app that displays corny dad jokes.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'About the App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/jokes/_id/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nuxt-link to="/jokes">Back to Jokes</nuxt-link>
+    <h2>{{ joke }}</h2>
+    <hr />
+    <small>Joke Id: {{$route.params.id}}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      joke: {}
+    }
+  },
+  async created () {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config)
+      this.joke = res.data.joke
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
